01b1369608d24087f5fc95ec3d67e750e274d431
sarah oliveira <sgeorgia0297@gmail.com>
2024-05-30 04:31:57 -0300

    vercel.json

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.',  // Define o diretório raiz como o diretório principal
+  base: './', // Define a base URL relativa
+  publicDir: 'public', // Diretório para arquivos estáticos
+  build: {
+    outDir: 'dist', // Diretório de saída para a build
+    rollupOptions: {
+      input: 'index.html', // Arquivo de entrada principal
+    }
+  },
+  server: {
+    open: true // Abre o navegador automaticamente quando o servidor iniciar
+  }
+});
